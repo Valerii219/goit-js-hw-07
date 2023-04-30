9703e857faa90e1e78fa6b1ce67719ba170d10de
Valerii219 <valera.arl96@gmail.com>
2023-04-30 23:06:06 +0200

    !

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+function createGalleryItemes(galleryItems){
+    const markup = galleryItems.map(galleryItems => {
+        return
+         `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+        />
+        </a>
+        </li>`;
+    });
+        console.log(markup);
+} 
 
-console.log(galleryItems);
+
+// console.log(galleryItems);
