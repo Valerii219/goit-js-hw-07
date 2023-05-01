95103414693406a040c0bd73398a951fff6fd867
Valerii219 <valera.arl96@gmail.com>
2023-05-01 22:04:05 +0200

    !hj

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,21 +1,44 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
-function createGalleryItemes(galleryItems){
-    const markup = galleryItems.map(galleryItems => {
-        return
-         `<li class="gallery__item">
-        <a class="gallery__link" href="${original}">
+
+console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery')
+
+function createGalleryItemesCard(galleryItems){
+    
+        return galleryItems.map((galleryItem ) => 
+        `<li class="gallery__item">
+        <a class="gallery__link" href="${galleryItem.original}">
         <img
             class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
+            src="${galleryItem.preview}"
+            data-source="${galleryItem.original}"
+            alt=${galleryItem.description}
         />
         </a>
-        </li>`;
-    });
-        console.log(markup);
+        </li>`
+    ).join("")
+
 } 
+const itemesCard = createGalleryItemesCard(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend',itemesCard )
 
+galleryEl.addEventListener('click', onGalleryElClick)
 
-// console.log(galleryItems);
+function onGalleryElClick(evt){
+    evt.preventDefault();
+    if(evt.target.nodeName !== 'IMG'){
+        return;
+    }
+    const urlBigImage = evt.target.dataset.source;
+    const instance = basicLightbox.create(`
+        <img src="${urlBigImage}" width="800" height="600">
+    `)
+    instance.show()
+    
+    galleryEl.addEventListener("keydown", (evt) => {
+    if(evt.code === "Escape"){
+        instance.close();
+    }
+})
+}
