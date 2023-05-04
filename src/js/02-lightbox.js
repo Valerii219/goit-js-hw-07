36b1eea3ae12cdbe8de22a506ad5a9d36326bf57
Valerii219 <valera.arl96@gmail.com>
2023-05-04 21:00:02 +0200

    + new

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -1,4 +1,21 @@
 import { galleryItems } from './gallery-items.js';
+
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery')
+
+function createGallery(galleryItems){
+    
+    return galleryItems.map(({preview, original, description} ) =>
+    `<li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+    </a>
+ </li>`).join('')
+ }
+
+ const itemesCard = createGallery(galleryItems);
+galleryEl.insertAdjacentHTML('beforeend', itemesCard )
+new SimpleLightbox('.gallery', { animationSlide });
+
