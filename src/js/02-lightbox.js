7c1324d0300edd3c03552c7f23722ce530d6e4d4
Valerii219 <valera.arl96@gmail.com>
2023-05-05 20:07:13 +0200

    Finish homowork

--- src/js/02-lightbox.js
+++ src/js/02-lightbox.js
@@ -10,12 +10,12 @@ function createGallery(galleryItems){
     return galleryItems.map(({preview, original, description} ) =>
     `<li class="gallery__item">
         <a class="gallery__link" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="" title="${description}" />
+        <img class="gallery__image" src="${preview}" alt="${description}"   />
     </a>
  </li>`).join('')
  }
 
  const itemesCard = createGallery(galleryItems);
 galleryEl.insertAdjacentHTML('beforeend', itemesCard )
-new SimpleLightbox('.gallery', { animationSlide });
-
+const optionsEl = {captionData:'alt', captionDelay:'250'};
+const gallery = new SimpleLightbox('.gallery__link', optionsEl);
